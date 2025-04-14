7253030a25de6b63a106ee86743671efd3a9a020
Idespair <kawanptbr@gmail.com>
2025-04-14 20:10:26 -0300

    Controller basic functions.

--- src/main/java/com/tracker/tracker/controller/ChatRoomController.java
+++ src/main/java/com/tracker/tracker/controller/ChatRoomController.java
@@ -1,7 +1,13 @@
 package com.tracker.tracker.controller;
 
+import com.tracker.tracker.repository.IChatRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/chat")
 public class ChatRoomController {
+    @Autowired
+    private IChatRoomRepository iChatRoomRepository;
 }
