bac9db5fc47c3fbeb4f28bf6e293cb256cd59300
Idespair <kawanptbr@gmail.com>
2025-04-16 01:00:52 -0300

    User service layer set up.

--- src/main/java/com/tracker/tracker/service/UserService.java
+++ src/main/java/com/tracker/tracker/service/UserService.java
@@ -1,7 +1,44 @@
 package com.tracker.tracker.service;
 
+import com.tracker.tracker.dtos.UserDTO;
+import com.tracker.tracker.model.User;
+import com.tracker.tracker.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private IUserRepository iUserRepository;
+
+    public Mono<UserDTO> getUserByEmail(String email){
+        return iUserRepository.findByEmail(email)
+                .switchIfEmpty(Mono.error(new RuntimeException("No user found with email: " + email)))
+                .map(this::toUserDTO);
+    }
+
+    public Flux<UserDTO> getAllByCertainName(String name){
+        return iUserRepository.findAllByCertainName(name)
+                .switchIfEmpty(Flux.error(new RuntimeException("No user found with name: " + name)))
+                .map(this::toUserDTO);
+    }
+
+    public Flux<UserDTO> getAllByAssignedTasks(String taskId){
+        return iUserRepository.findAllByAssignedTask(taskId)
+                .switchIfEmpty(Flux.error(new RuntimeException("No tasks found with the ID: " + taskId)))
+                .map(this::toUserDTO);
+    }
+
+    public Flux<UserDTO> findAllByRole(String role){
+        return iUserRepository.findAllByCertainRole(role)
+                .switchIfEmpty(Flux.error(new RuntimeException("No user found with role: " + role)))
+                .map(this::toUserDTO);
+    }
+
+    private UserDTO toUserDTO (User user){
+        return new UserDTO(user.getId(), user.getName(), user.getRole(), user.getEmail());
+    }
 }
