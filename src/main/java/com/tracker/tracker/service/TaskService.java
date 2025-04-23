9c2bfe2354256a116ad2d0943f38bd419149047c
Idespair <kawanptbr@gmail.com>
2025-04-23 16:00:01 -0300

    TaskService updated.

--- src/main/java/com/tracker/tracker/service/TaskService.java
+++ src/main/java/com/tracker/tracker/service/TaskService.java
@@ -2,6 +2,7 @@ package com.tracker.tracker.service;
 
 import com.tracker.tracker.dtos.TaskDTO;
 import com.tracker.tracker.exceptions.InvalidTaskException;
+import com.tracker.tracker.exceptions.InvalidUserException;
 import com.tracker.tracker.model.Task;
 import com.tracker.tracker.repository.ITaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,20 @@ public class TaskService {
     public Flux<TaskDTO> getByOpenTasks (Boolean isOpen){
         return iTaskRepository.findTaskByOpenedTasks(isOpen)
                 .switchIfEmpty(Mono.error(new InvalidTaskException("No tasks found")))
+                .map(this::taskToDto);
+    }
+
+    public Mono<TaskDTO> createNewTask (Task task){
+
+        if (task.getId() != null){
+            return Mono.error(new InvalidTaskException("Task must not have any ID before creation"));
+        }
+
+        if (task.getTitle() == null || task.getTitle().isBlank()){
+            return Mono.error(new InvalidTaskException("Task must have a title"));
+        }
+
+        return iTaskRepository.save(task)
                 .map(this::taskToDto);
     }
 
