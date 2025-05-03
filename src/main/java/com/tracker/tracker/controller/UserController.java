fd8f78921d7b7a7717cde29e6b10a5c544a67d35
Idespair <kawanptbr@gmail.com>
2025-05-02 23:12:17 -0300

    Controller updated.

--- src/main/java/com/tracker/tracker/controller/UserController.java
+++ src/main/java/com/tracker/tracker/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.tracker.tracker.controller;
 
+import com.tracker.tracker.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
 }
