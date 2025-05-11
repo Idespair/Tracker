84b3713232c41930b64c80a254573d202e4f58db
Idespair <kawanptbr@gmail.com>
2025-05-10 23:55:59 -0300

    Controller updated.

--- src/main/java/com/tracker/tracker/controller/MessageController.java
+++ src/main/java/com/tracker/tracker/controller/MessageController.java
@@ -1,7 +1,12 @@
 package com.tracker.tracker.controller;
 
+import com.tracker.tracker.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
 }
